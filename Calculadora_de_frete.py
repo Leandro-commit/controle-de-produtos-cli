f16986c1a8ac74c1182d5947659557ec59e140ca
Leandro Alves <leandro.commit@gmail.com>
2026-04-09 19:03:49 -0300

    Adicionado exercícios de calculadora e frete

--- Calculadora_de_frete.py
+++ Calculadora_de_frete.py
@@ -1,8 +1,45 @@
-peso = float(input("Insira o peso do pacote: "))
+def moeda(valor):
+    return f"{valor:.2f}".replace(".", ",")
+
+
+peso = float(input("Qual o peso do pacote em kg: "))
 print("-" * 30)
 print("Tipo de envio:\n")
 print("E. Econômico")
 print("N. Normal")
 print("EX. Expresso\n")
-envio = input("Escolha a letra correspondente (E, N ou EX): ")
+envio = input("Escolha a letra correspondente (E, N ou EX): ").upper()
 print("-" * 30)
+
+if envio == "E":
+    if peso <= 5:
+        frete = peso * 0.50
+        print("Faixa de peso: Até 5kg")
+    else:
+        frete = peso * 0.80
+        print("Faixa de peso: Acima 5kg")
+    print("Tipo de envio: Econômico")
+    print(f"Valor do frete: R$ {moeda(frete)}")
+
+elif envio == "N":
+    if peso <= 5:
+        frete = peso * 0.90
+        print("Faixa de peso: Até 5kg")
+    else:
+        frete = peso * 1.20
+        print("Faixa de peso: Acima de 5kg")
+    print("Tipo de envio: Normal")
+    print(f"Valor do frete: R$ {moeda(frete)}")
+
+elif envio == "EX":
+    if peso <= 5:
+        frete = peso * 1.50
+        print("Faixa de peso: Até 5kg")
+    else:
+        frete = peso * 2.00
+        print("Faixa de peso: Acima de 5kg")
+    print("Tipo de envio: Expresso")
+    print(f"Valor do frete: R$ {moeda(frete)}")
+
+else:
+    print("Opção inválida!")
---
+++ Calculadora_loja.py
@@ -0,0 +1,73 @@
+def moeda(valor):
+    return f"{valor:.2f}".replace(".", ",")
+
+
+compra = float(input("Digite o valor da compra: R$ ").replace(",", "."))
+print("-" * 30)
+print("\nE. Eletrônicos")
+print("V. Vestuário")
+print("A. Alimentos\n")
+
+categoria = input("Qual a categoria do produto? ").upper()
+
+if categoria == "E":
+    if compra <= 100:
+        desconto = compra * 0.05
+        print("Categoria: Eletrônicos")
+        print("Faixa: Até R$ 100,00")
+        print("Desconto: 5%")
+
+    elif compra <= 500:
+        desconto = compra * 0.10
+        print("Faixa: de R$ 100,00 à R$ 500,00")
+        print("Desconto: 10%")
+
+    else:
+        desconto = compra * 0.20
+        print("Faixa: Acima de R$ 500,00")
+        print("Desconto: 20%")
+    print(f"Valor do desconto: R$ {moeda(desconto)}")
+    print(f"Valor final: R$ {moeda(compra - desconto)}")
+
+elif categoria == "V":
+    if compra <= 100:
+        desconto = compra * 0
+        print("Categoria: Vestuário")
+        print("Faixa: Até R$ 100,00")
+        print("Sem desconto!")
+
+    elif compra <= 500:
+        desconto = compra * 0.05
+        print("Categoria: Vestuário")
+        print("Faixa: De R$ 100,00 à R$ 500,00")
+        print("Desconto: 10%")
+
+    else:
+        desconto = compra * 0.15
+        print("Categoria: Vestuário")
+        print("Faixa: Acima de R$ 500,00")
+        print("Desconto: 10%")
+    print(f"Valor do desconto: R$ {moeda(desconto)}")
+    print(f"Valor final: R$ {moeda(compra - desconto)}")
+
+elif categoria == "A":
+    if compra <= 100:
+        desconto = compra * 0.02
+        print("Categoria: Alimentos")
+        print("Faixa: Até R$ 100,00")
+        print("Desconto: 2%")
+    elif compra <= 500:
+        desconto = compra * 0.05
+        print("Categoria: Alimentos")
+        print("Faixa: De R$ 100,00 à R$ 500,00")
+        print("Desconto: 5%")
+    else:
+        desconto = compra * 0.08
+        print("Categoria: Alimentos")
+        print("Faixa: Acima de R$ 500,00")
+        print("Desconto: 8%")
+    print(f"Valor do desconto: R$ {moeda(desconto)}")
+    print(f"Valor final: R$ {moeda(compra - desconto)}")
+
+else:
+    print("Opção inválida")
