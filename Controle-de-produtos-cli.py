e85f6058362b44b96bd14b308dd75cd6444d16fb
Leandro Alves <leandro.commit@gmail.com>
2026-08-10 23:18:18 -0300

    Refatoração do código com evolução do que aprendi

--- Controle-de-produtos-cli.py
+++ Controle-de-produtos-cli.py
@@ -1,54 +1,127 @@
-carrinho = []
+produtos = []
+
+
+def mostrar_menu():
+    print("===Controle de produtos===\n")
+    print("1. Cadastrar produto")
+    print("2. Listar ou remover produtos")
+    print("3. Buscar produto")
+    print("4. SAIR\n")
+
+
+def cadastrar_produto():
+
+    nome = input("Nome do produto: ")
+    while True:
+        try:
+            preco_conversao = input("Preço: ").replace(",", ".")
+            preco = float(preco_conversao)
+            if preco <= 0:
+                print("Preço inválido")
+                continue
+            break
+        except ValueError:
+            print("Digite um valor válido!")
+
+    while True:
+        try:
+            qtd = int(input("Quantidade: "))
+            if qtd <= 0:
+                print("Digite uma quantidade acima de 0")
+                continue
+            break
+
+        except ValueError:
+            print("Digite uma quantidade válida!")
+            continue
+
+    cadastro = {"nome": nome, "preco": preco, "qtd": qtd}
+
+    produtos.append(cadastro)
+    print("\033[92mProduto cadastrado com sucecesso!\n\033[0m")
+
+
+# def listar_produtos():
+#     if not produtos:
+#         print("Nenhum produto cadastrado!")
+#         return
+
+#     for produto in produtos:
+#         print(
+#             f"Nome: {produto['nome']} \n"
+#             f"Preço: {f'{produto['preco']:.2f}'.replace('.', ',')} \n"
+#             f"Quantidade: {produto['qtd']}"
+#         )
+#         print("-" * 20)
+
+
+def buscar_produto():
+    nome = input("Digite o nome do produto que deseja buscar: ")
+    encontrado = False
+
+    for produto in produtos:
+        if produto["nome"].lower() == nome.lower():
+            print("\033[92m\n===Produto encontrado===\033[m")
+            print(
+                f"Nome: {produto['nome']} | Preço: {produto['preco']:.2f} | Quantidade: {produto['qtd']}"
+            )
+            encontrado = True
+            break
+
+    if not encontrado:
+        print("\033[91mProduto não encontrado\033[0m")
+
+
+def listar_remover():
+    if not produtos:
+        print("\nNenhum produto registrado!")
+        return
+
+    else:
+        print("\nProdutos cadastrados:")
+        for i, item in enumerate(produtos, start=1):
+            print(
+                f"\n{i} - Nome: {item['nome']} | "
+                f"Preço: {f'{item['preco']:.2f}'.replace('.', ',')} | "
+                f"Quantidade: {item['qtd']}"
+            )
+
+        remover = int(
+            input("\nDigite 0 para SAIR ou o índice do produto que deseja remover: ")
+        )
+        if remover >= 1 and remover <= len(produtos):
+            produto_removido = produtos.pop(remover - 1)
+            print(f"\n{produto_removido['nome']} foi removido do carrinho!\n")
+
+        elif remover == 0:
+            print("Voltando ao menu...\n")
+            return
+        else:
+            print("\nNúmero inválido")
+
 
 while True:
+    mostrar_menu()
 
-    print("\n1 - Registrar produto")
-    print("2 - Listar e remover produtos")
-    print("3 - Sair\n")
-
-    # ---INVÁLIDAR INSERÇÃO DE LETRAS E CÓDIGOS---
     try:
         opcao = int(input("Escolha uma opção: "))
+        if opcao not in range(1, 5):
+            print("Opção inválida")
+            continue
+
     except ValueError:
-        print("Digite apenas números!")
+        print("Digite uma opção válida!")
         continue
 
-    # ---INSERIR PRODUTO---
     if opcao == 1:
-        produto = input("Nome do produto: ")
-        preco = float(input("Preço: ").replace(",", "."))
-        qtd = int(input("Quantidade: "))
+        cadastrar_produto()
 
-        registro = {"nome": produto, "preco": preco, "quantidade": qtd}
-        carrinho.append(registro)
-        print("\nProduto adicionado!")
-
-    # ---LISTAGEM E REMOÇÃO---
     elif opcao == 2:
-        if not carrinho:
-            print("\nNenhum produto registrado.")
+        listar_remover()
 
-        else:
-            print("\nProdutos no carrinho:")
-            for i, item in enumerate(carrinho, start=1):
-                print(
-                    f"\n{i} - Nome: {item['nome']} | "
-                    f"Preço: R$ {item['preco']:.2f}".replace(".", ",")
-                    + f" | Quantidade: {item['quantidade']}"
-                )
-            print("\nDigite 0 para sair ou...")
-            remover = int(input("Digite o número do produto que deseja remover: "))
-
-            if remover >= 1 and remover <= len(carrinho):
-                produto_removido = carrinho.pop(remover - 1)
-                print(f"\n{produto_removido['nome']} foi removido do carrinho!")
-            elif remover == 0:
-                print("Voltando ao menu...")
-            else:
-                print("\nNúmero inválido")
-
-    # ---SAIR E INVALIDAÇÃO---
     elif opcao == 3:
+        buscar_produto()
+
+    elif opcao == 4:
+        print("Encerrando...")
         break
-    else:
-        print("\nOpção inválida. Tente novamente.")
